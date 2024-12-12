eb478ee849269a7e431a215c130047de695cd305
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-12-12 22:16:48 +0100

    Server loop to finish

--- obj/Sources/Server/serverDashboard.d
+++
@@ -1,3 +0,0 @@
-obj/Sources/Server/serverDashboard.o: Sources/Server/serverDashboard.cpp \
- Includes/../Includes/Server.hpp Includes/../Includes/utils.hpp \
- Includes/../Includes/Color.hpp Includes/../Includes/Client.hpp
--- obj/main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: main.cpp Includes/Server.hpp Includes/utils.hpp \
- Includes/Color.hpp Includes/Client.hpp
