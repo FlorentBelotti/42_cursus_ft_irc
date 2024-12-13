66262accbbbe7ecc67a4c01cb50eac7f8e0176ae
FlorentBelotti <florent.l.d.belotti@gmail.com>
2024-12-13 15:59:56 +0100

    client class

--- obj/Sources/Server/serverSocket.d
+++
@@ -1,3 +0,0 @@
-obj/Sources/Server/serverSocket.o: Sources/Server/serverSocket.cpp \
- Includes/../Includes/Server.hpp Includes/../Includes/utils.hpp \
- Includes/../Includes/Color.hpp Includes/../Includes/Client.hpp
