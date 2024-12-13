1439753d83c0007c834ffd2b3382d88e96b7389f
FlorentBelotti <florent.l.d.belotti@gmail.com>
2024-12-13 15:59:48 +0100

    client class

--- Includes/Client.hpp
+++ Includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: fbelotti <fbelotti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:42:49 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/11/29 16:08:40 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/12/13 15:59:37 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,54 @@ class Client {
     
     private:
     
-    // Client informations
+    // Client's status
+    
+        bool    _isLogged;
+        bool    _isBot;
+        int     _pswdTries;
+    
+    // Client's connexion
     
         int         _clientFd;
         std::string _clientIp;
+
+    // Client's informations
+
+        std::string _hostname;
+        std::string _nickname;
+        std::string _username;
+        std::string _buffer;
     
     public:
     
     // Constructor
         
-        Client();
+        Client(int newClientFd, std::string host);
+
+    // Destructor
+
+        virtual ~Client();
 
     // Getters
         
         int getClientFd() const;
-        int getClientIp() const;
-
+        int getClientHostname() const;
+        int getClientNickname() const;
+        int getClientUsername() const;
+        int getClientBuffer() const;
+        int getClientBotStatus() const;
+        int getClientLogStatus() const;
+        int getClientPswdTries() const;
+        
     // Setters
     
         void setClientFd(int clientFd);
         void setClientIp(std::string clientIp);
+        void setClientHostname(std::string hostname) const;
+        void setClientNickname(std::string nickname) const;
+        void setClientUsername(std::string username) const;
+        void setClientBuffer(std::string buffer) const;
+        void setClientBotStatus(bool status) const;
+        void setClientLogStatus(bool status) const;
+        void setClientPswdTries(bool status) const;
 };
---
+++ obj/Sources/Server/serverDashboard.d
@@ -0,0 +1,3 @@
+obj/Sources/Server/serverDashboard.o: Sources/Server/serverDashboard.cpp \
+ Includes/../Includes/Server.hpp Includes/../Includes/utils.hpp \
+ Includes/../Includes/Color.hpp Includes/../Includes/Client.hpp
