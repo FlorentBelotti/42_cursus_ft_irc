ae8672d0babd65227b83a83de8c2f0f653f9dad8
Florent Belotti <florent.l.d.belotti@gmail.com>
2025-01-09 19:18:16 +0100

    irc final push

---
+++ obj/Sources/Server/serverSocket.d
@@ -0,0 +1,12 @@
+obj/Sources/Server/serverSocket.o: Sources/Server/serverSocket.cpp \
+ Sources/Server/../../Includes/Server.hpp \
+ Sources/Server/../../Includes/utils.hpp \
+ Sources/Server/../../Includes/Color.hpp \
+ Sources/Server/../../Includes/Client.hpp \
+ Sources/Server/../../Includes/../Includes/Channel.hpp \
+ Sources/Server/../../Includes/../Includes/utils.hpp \
+ Sources/Server/../../Includes/../Includes/Color.hpp \
+ Sources/Server/../../Includes/../Includes/Server.hpp \
+ Sources/Server/../../Includes/../Includes/Server.hpp \
+ Sources/Server/../../Includes/Bot.hpp \
+ Sources/Server/../../Includes/Server.hpp
---
+++ obj/main.d
@@ -0,0 +1,4 @@
+obj/main.o: main.cpp Includes/Server.hpp Includes/utils.hpp \
+ Includes/Color.hpp Includes/Client.hpp Includes/../Includes/Channel.hpp \
+ Includes/../Includes/utils.hpp Includes/../Includes/Color.hpp \
+ Includes/../Includes/Server.hpp Includes/../Includes/Server.hpp
